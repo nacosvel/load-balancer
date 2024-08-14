17292f51523c226434b7b0c83358345caa1ff4d9
jundayw <jundayw@126.com>
2024-08-14 21:13:24 +0800

    feat:RandomRule

--- src/Rules/RandomRule.php
+++ src/Rules/RandomRule.php
@@ -3,12 +3,25 @@
 namespace Nacosvel\LoadBalancer\Rules;
 
 use Nacosvel\LoadBalancer\Contracts\LoadBalancerRuleInterface;
+use Nacosvel\LoadBalancer\Contracts\ServerInstanceInterface;
+use Nacosvel\LoadBalancer\Server\ServerInstance;
 
 class RandomRule extends AbstractLoadBalancerRule implements LoadBalancerRuleInterface
 {
-    public function choose(int $key = 1): static
+    public function choose(int $key = 0): ServerInstanceInterface
     {
-        return $this;
+        if (array_key_exists($key, ServerInstance::$hashCodeTable) && ServerInstance::$hashCodeTable[$key]->isAlive()) {
+            return ServerInstance::$hashCodeTable[$key];
+        }
+
+        $reachableServers = $this->getLoadBalancer()->getReachableServers();
+        $range            = range(0, $reachableServers->count() - 1, 1);
+        $random           = array_rand($range);
+        $reachableServers->seek($random);
+
+        assert($reachableServers->current() instanceof ServerInstanceInterface);
+
+        return $reachableServers->current();
     }
 
 }
