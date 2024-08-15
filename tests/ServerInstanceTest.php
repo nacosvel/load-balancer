984af580b401994e7b3b455ae7d6edc8129b863a
jundayw <jundayw@126.com>
2024-08-15 09:14:05 +0800

    test

---
+++ tests/ServerInstanceTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Nacosvel\LoadBalancer\Test;
+
+use PHPUnit\Framework\TestCase;
+
+class ServerInstanceTest extends TestCase
+{
+    public function testTrue()
+    {
+        $this->assertTrue(true);
+    }
+
+}
