c0c75b97c1ce5789d36529ae453a5a528f332aff
jundayw <jundayw@126.com>
2024-08-15 14:17:30 +0800

    perf:RandomRule

--- src/Rules/RandomRule.php
+++ src/Rules/RandomRule.php
@@ -2,14 +2,20 @@
 
 namespace Nacosvel\LoadBalancer\Rules;
 
+use Exception;
 use Nacosvel\LoadBalancer\Contracts\LoadBalancerRuleInterface;
 use Nacosvel\LoadBalancer\Contracts\ServerInstanceInterface;
 use Nacosvel\LoadBalancer\Exceptions\InvalidArgumentExceptions;
+use Nacosvel\LoadBalancer\Exceptions\LoadBalancerExceptions;
 use Nacosvel\LoadBalancer\Exceptions\UnderflowExceptions;
 use Nacosvel\LoadBalancer\Server\ServerInstance;
+use OutOfBoundsException;
 
 class RandomRule extends AbstractLoadBalancerRule implements LoadBalancerRuleInterface
 {
+    /**
+     * @throws LoadBalancerExceptions
+     */
     public function choose(int $key = 0): ServerInstanceInterface
     {
         if (array_key_exists($key, ServerInstance::$hashCodeTable) && ServerInstance::$hashCodeTable[$key]->isAlive()) {
@@ -21,11 +27,12 @@ class RandomRule extends AbstractLoadBalancerRule implements LoadBalancerRuleInt
         assert($reachableServers->count(), new UnderflowExceptions('There are no more reachable servers available'));
 
         try {
-            $reachableServers->seek($this->getRandomNumber($reachableServers->count()));
-        } catch (\Throwable $throwable) {
-            if (false === $reachableServers->valid()) {
-                return $this->choose($key);
-            }
+            $offset = $this->getRandomNumber($reachableServers->count());
+            $reachableServers->seek($offset);
+        } catch (OutOfBoundsException $exception) {
+            return $this->choose($key);
+        } catch (Exception $exception) {
+            throw new LoadBalancerExceptions($exception->getMessage(), $exception->getCode(), $exception->getPrevious());
         }
 
         assert($reachableServers->current() instanceof ServerInstanceInterface, new InvalidArgumentExceptions('reachable server instance implements interface error'));
