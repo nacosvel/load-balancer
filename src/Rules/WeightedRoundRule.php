a9d7f59782a708099c405766b75ecabacb277c0a
jundayw <jundayw@126.com>
2024-08-16 16:55:10 +0800

    CI

---
+++ src/Rules/WeightedRoundRule.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Nacosvel\LoadBalancer\Rules;
+
+use Nacosvel\LoadBalancer\Contracts\LoadBalancerRuleInterface;
+use Nacosvel\LoadBalancer\Contracts\ServerInstanceInterface;
+use Nacosvel\LoadBalancer\Exceptions\InvalidArgumentExceptions;
+use Nacosvel\LoadBalancer\Exceptions\LoadBalancerExceptions;
+use Nacosvel\LoadBalancer\Exceptions\UnderflowExceptions;
+use Nacosvel\LoadBalancer\Server\ServerInstance;
+
+/**
+ * @deprecated
+ */
+class WeightedRoundRule extends AbstractLoadBalancerRule implements LoadBalancerRuleInterface
+{
+    /**
+     * @throws LoadBalancerExceptions
+     */
+    public function choose(int $key = 0): ServerInstance
+    {
+        $reachableServers = $this->getLoadBalancer()->getReachableServers();
+
+        assert($reachableServers->count(), new UnderflowExceptions('There are no more reachable servers available'));
+
+        $reachableServers->rewind();
+
+        assert($reachableServers->current() instanceof ServerInstanceInterface, new InvalidArgumentExceptions('reachable server instance implements interface error'));
+
+        return $reachableServers->current();
+    }
+
+}
