b27112cd185a6a00031bcb2e91f092611348228b
jundayw <jundayw@126.com>
2024-08-22 16:55:33 +0800

    fix:PHP8.0

--- src/Contracts/ArrayIteratorInterface.php
+++ src/Contracts/ArrayIteratorInterface.php
@@ -6,24 +6,23 @@ use SeekableIterator;
 
 interface ArrayIteratorInterface extends SeekableIterator
 {
-    public function offsetExists(mixed $key): bool;
+    // public function offsetExists(mixed $key): bool;
 
     public function offsetGet(mixed $key): ServerInstanceInterface;
 
     public function offsetSet(int $key, mixed $value): void;
 
-    public function offsetUnset(mixed $key): void;
+    // public function offsetUnset(mixed $offset): void;
 
     public function append(string $value): void;
 
-    public function getArrayCopy(): array;
+    // public function getArrayCopy(): array;
 
-    public function count(): int;
+    // public function count(): int;
 
-    public function unserialize(string $data): void;
+    // public function unserialize(string $data): void;
 
-    public function serialize(): string;
-
+    // public function serialize(): string;
 
     public function current(): ServerInstanceInterface;
 
